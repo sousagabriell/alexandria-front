16ce852d8f13e9ed8f96d432370a8565323bdc00
Gabriel Sousa <gabrieldesousa05@gmail.com>
2022-05-26 04:08:00 -0300

    tela de login finalizada com mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './tampletes/login/login.component';
+import { DashboardComponent } from './tampletes/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
